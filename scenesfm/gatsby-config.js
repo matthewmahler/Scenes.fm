a940e1621c7a744cd0219590a5f9368384c4d77f
matthewmahler <matthewjamesmahler@gmail.com>
2019-02-28 19:36:39 -0500

    placeholder site

--- scenesfm/gatsby-config.js
+++ scenesfm/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Scenes.FM`,
+    description: `A music streaming service built by artists.`,
+    author: `Matt Mahler`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,15 +18,32 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-scenes-placeholder`,
+        short_name: `scenes.fm`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2d2d2d`,
+        _color: `#2d2d2d`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Pacifico`,
+            variants: [`400`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`],
+          },
+        ],
+      },
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
--- scenesfm/src/pages/index.js
+++ scenesfm/src/pages/index.js
@@ -1,20 +1,75 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import styled from "styled-components"
+import background from "../images/bg.jpg"
+
+const Hero = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  height: 100vh;
+  background: url(${background}) no-repeat center center;
+  background-size: cover !important;
+  align-content: center;
+  grid-template-rows: 1fr auto;
+  div.hero {
+    display: grid;
+    grid-template-columns: 1fr;
+    align-content: center;
+  }
+  h1 {
+    font-family: "Pacifico", cursive;
+    text-align: center;
+    font-size: 6rem;
+    color: #39bace;
+    text-shadow: 10px 10px 10px #000;
+  }
+  h4 {
+    font-family: "Open Sans", sans-serif;
+    text-align: center;
+    font-size: 2rem;
+    color: #39bace;
+    text-shadow: 5px 5px 5px #000;
+  }
+  p {
+    font-family: "Open Sans", sans-serif;
+    text-align: center;
+    font-size: 1rem;
+    margin: 0 auto;
+    color: #39bace;
+    text-shadow: 5px 5px 5px #000;
+  }
+  footer {
+    font-family: "Open Sans", sans-serif;
+    text-align: center;
+    font-size: 1rem;
+    margin: 0 auto;
+    color: #39bace;
+    text-shadow: 5px 5px 5px #000;
+    a {
+      text-decoration: none;
+      color: #39bace;
+    }
+  }
+`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Hero>
+      <div className="hero">
+        <h1>Scenes.FM</h1>
+        <h4>Revolutionizing discovering and listening to local music.</h4>
+        <p>(Under Construction)</p>
+      </div>
+
+      <footer>
+        © {new Date().getFullYear()}, Built by
+        {` `}
+        <a href="https://www.mattmahler.tech">Matt Mahler</a>
+      </footer>
+    </Hero>
   </Layout>
 )
 
